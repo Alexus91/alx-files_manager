74c839359c98e3e14a116c66cd3dfbbbc37408a7
Alexus91 <mrfadlkamal@gmail.com>
2024-03-13 01:56:22 -0700

    Files controller and index

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,34 @@
+/* eslint-disable import/no-named-as-default */
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
-import FilesController from '../controllers/FilesController'; // Import the FilesController
+import FilesController from '../controllers/FilesController';
 
 const router = Router();
 
 router.get('/status', AppController.getStatus);
+
 router.get('/stats', AppController.getStats);
 
-// User endpoints
 router.post('/users', UsersController.postNew);
 
-// Authentication endpoints
 router.get('/connect', AuthController.getConnect);
+
 router.get('/disconnect', AuthController.getDisconnect);
+
 router.get('/users/me', UsersController.getMe);
 
-// Files endpoints
-router.post('/files', FilesController.postUpload); // Add the new endpoint for uploading files
+router.post('/files', FilesController.postUpload);
 
-export default router;
+router.get('/files/:id', FilesController.getShow);
+
+router.get('/files', FilesController.getIndex);
+
+router.put('/files/:id/publish', FilesController.putPublish);
+
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
+router.get('/files/:id/data', FilesController.getFile);
+
+module.exports = router;
